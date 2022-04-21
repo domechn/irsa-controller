15b7f00b002c633c109bf30c86d1aa3b94b5b637
domgoer <domdoumc@gmail.com>
2022-04-22 01:34:03 +0800

    init

---
+++ api/v1alpha1/iamroleserviceaccount_types.go
@@ -0,0 +1,90 @@
+/*
+Copyright 2022 domechn.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// IamRoleServiceAccountSpec defines the desired state of IamRoleServiceAccount
+type IamRoleServiceAccountSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Policy PolicySpec `json:"policy"`
+}
+
+type PolicySpec struct {
+	// +optional
+	ARN       string          `json:"arn,omitempty"`
+	Statement []StatementSpec `json:"statement"`
+}
+
+type StatementSpec struct {
+	Resource []string `json:"resource"`
+	Action   []string `json:"action"`
+}
+
+// IamRoleServiceAccountStatus defines the observed state of IamRoleServiceAccount
+type IamRoleServiceAccountStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	// +optional
+	Condition IrsaCondition `json:"condition"`
+	// +optional
+	Reason string `json:"reason,omitempty"`
+}
+
+type IrsaCondition string
+
+var (
+	IrsaSubmitted      IrsaCondition = ""
+	IrsaPending        IrsaCondition = "pending"
+	IrsaSaNameConflict IrsaCondition = "saNameConflict"
+	IrsaForbidden      IrsaCondition = "forbidden"
+	IrsaFailed         IrsaCondition = "failed"
+	IrsaProgressing    IrsaCondition = "progressing"
+	IrsaOK             IrsaCondition = "created"
+)
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// IamRoleServiceAccount is the Schema for the iamroleserviceaccounts API
+type IamRoleServiceAccount struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   IamRoleServiceAccountSpec   `json:"spec,omitempty"`
+	Status IamRoleServiceAccountStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// IamRoleServiceAccountList contains a list of IamRoleServiceAccount
+type IamRoleServiceAccountList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []IamRoleServiceAccount `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&IamRoleServiceAccount{}, &IamRoleServiceAccountList{})
+}
